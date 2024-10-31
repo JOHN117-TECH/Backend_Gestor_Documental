48c87c1ecfbf5d21da59b6b6167d4ce97a2bb328
John <barretodiazjohnalexander@gmail.com>
2024-10-30 20:27:03 -0500

    se agrego url DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { Files } from './files/entities/file.entity';
     TypeOrmModule.forFeature([Files]),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
+      host: process.env.DB_BASE_URL,
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
